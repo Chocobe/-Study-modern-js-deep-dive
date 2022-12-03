fa20bb96a95509e2daf1a81de7347a7c7ee562ef
김영우 <kyw05171@gmail.com>
2022-12-03 16:52:24 +0900

    12장. 함수 정리

--- 12장-함수/12-3_함수_리터럴.js
+++ 12장-함수/12-3_함수_리터럴.js
@@ -20,3 +20,37 @@ function div(a, b) {
 
 const myCalc = new Function('a', 'b', 'return a + b');
 console.log(`Use BuiltInFunction: ${myCalc(10, 20)}`);
+
+function doSome(name, age) {
+  console.log(arguments);
+}
+doSome("Chocobe", 36, "FrontEnd");
+
+(function() {
+  console.log("IIF: Immediately Invoked Function");
+}());
+
+function myPrint(num) {
+  if (num < 1) return;
+
+  console.log(num);
+  myPrint(num - 1);
+}
+myPrint(10);
+
+// HOF
+function repeat(value, callback) {
+  for (let i = 0; i < value; i++) {
+    callback(i);
+  }
+}
+
+function print(value) {
+  console.log(value);
+}
+repeat(5, print);
+
+function printOdd(value) {
+  if (value % 2 === 1) console.log(value);
+}
+repeat(5, printOdd);
